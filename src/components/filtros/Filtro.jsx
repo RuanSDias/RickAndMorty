53c7e253cd492f84cc016776dfb105a34d09db0e
Ruan Santos Dias <sdias.ruan@gmail.com>
2023-08-30 19:39:58 -0300

    Acordeao posicionado

--- src/components/filtros/Filtro.jsx
+++ src/components/filtros/Filtro.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import './categorias/Filtro.css'
+import './Filtro.css'
+import Genero from './categorias/Genero'
+import Especie from './categorias/Especie'
+import Status from './categorias/Status'
 
 const Filtro = () => {
   return (
     <div className="col-3">
       <div className='text-center fw-bold fs-4 mb-4'>Filtro</div>
       <div className="limparFiltro text-center text-primary text-decoration-underline">Limpar filtro</div>
+      <div className="accordion" id="accordionExample">
+        <Genero/>
+        <Especie/>
+        <Status/>
+      </div>
     </div>
   )
 }
