70d8aaaefd5939d8fdf40657e8ea8aec0bf423eb
Ruan Santos Dias <sdias.ruan@gmail.com>
2023-08-30 19:31:51 -0300

    Antes de colocar acordeao

--- src/components/filtros/Filtro.jsx
+++ src/components/filtros/Filtro.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './categorias/Filtro.css'
 
 const Filtro = () => {
   return (
     <div className="col-3">
-      <div>Filtro</div>
+      <div className='text-center fw-bold fs-4 mb-4'>Filtro</div>
+      <div className="limparFiltro text-center text-primary text-decoration-underline">Limpar filtro</div>
     </div>
   )
 }
