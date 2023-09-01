73caf1d5fc66e42a2628e165a1802c6dab95b7ed
Ruan Santos Dias <sdias.ruan@gmail.com>
2023-09-01 14:49:10 -0300

    Navbar finalizada

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav className="navbar justify-content-center navbar-expand-lg bg-body-tertiary">
-            <div className="justify-content-center">
-                <Link to='/' className="navbar-brand" href="#">
-                    <h1 className="logo">Rick and Morty Wiki</h1>
+        <nav className="navbar navbar-expand-lg bg-light navbar-light mb-4">
+            <div className="container">
+                <Link to='/' className="fs-3 ubuntu navbar-brand">
+                    Rick and Morty Wiki
                 </Link>
                 <button 
                 className="navbar-toggler" 
@@ -17,12 +18,24 @@ const Navbar = () => {
                 aria-expanded="false" 
                 aria-label="Toggle navigation"
                 >
+                    <style jsx = 'true'>
+                        {`
+                            button[aria-expanded='false'] > .fechar {
+                                display: none;
+                            }
+                            button[aria-expanded='true'] > .abrir {
+                                display: none;
+                            }
+                        `}
+                    </style>
+                    <i className="fa-solid fa-bars fw-bold text-dark abrir"></i>
+                    <i className="fa-solid fa-xmark fw-bold text-dark fechar"></i>
                 </button>
-                <div className="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <NavLink to='/' className="nav-link active fs-5">Personagens</NavLink>
-                        <NavLink to='/episodios' className="nav-link fs-5">Episodios</NavLink>
-                        <NavLink to='/locais' className="nav-link fs-5">Locais</NavLink>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div className="navbar-nav fs-5">
+                        <NavLink to='/' className="nav-link {({isActive}) => (isActive ? 'active' : null)}">Personagens</NavLink>
+                        <NavLink to='/episodios' className="nav-link">Episódios</NavLink>
+                        <NavLink to='/locais' className="nav-link">Locais</NavLink>
                     </div>
                 </div>
             </div>
--- src/paginas/Episodios.jsx
+++ src/paginas/Episodios.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Cards from '../components/cards/Card'
 
 const Episodios = () => {
+
+  const [id, setId] = useState(1)
+  const [info, setInfo] = useState([])
+  const [results, setResults] = useState([])
+  const { air_date, name } = info;
+
+
+  const api = `https://rickandmortyapi.com/api/episode/${id}`
+
+  useEffect(() => {
+    (async function(){
+
+      const data = await fetch(api).then((res) => res.json());
+      setInfo(data);
+
+      const characterList = await Promise.all(
+        data.characters.map((e) => {
+          return fetch(e).then((resp) => resp.json());
+        })
+      );
+      setResults(characterList);
+    })()
+  }, [api])
   return (
-    <div>Episodios</div>
+    <div className='container'>
+      <div className='row'>
+        <h1 className="text-center mb-4">
+          Episódio: {name === '' ? "Episódio" : name}
+        </h1>
+        <h5 className="text-center">
+          Data de exibição = {air_date === '' ? 'Data desconhecida' : air_date}
+        </h5>
+      </div>
+      <div className='row'>
+        <div className='col-3'>Pick episode</div>
+        <div className="col-8">
+          <div className="row">
+            
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
